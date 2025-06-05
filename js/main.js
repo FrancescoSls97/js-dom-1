4217cf4d149c3c789e4f4ce622eee4f5a0bec97a
FrancescoSls97 <frankds1997@gmail.com>
2025-06-05 17:07:45 +0200

    add button event log

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//selezione bottone
+
+const ligth_button = document.getElementById("light_button");
+
+// funzione evento per il bottone
+
+ligth_button.addEventListener("click", function () {
+  console.log("Hai cliccato");
+});
