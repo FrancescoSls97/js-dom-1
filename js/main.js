376273394bff5f5320a007aae5874e11a57a1d81
FrancescoSls97 <frankds1997@gmail.com>
2025-06-05 17:34:22 +0200

    complete exercise

--- js/main.js
+++ js/main.js
@@ -2,8 +2,18 @@
 
 const ligth_button = document.getElementById("light_button");
 
+//selezione elementi
+
+const turn_off = document.getElementById("white_bulb");
+const turn_on = document.getElementById("yellow_bulb");
+
 // funzione evento per il bottone
 
 ligth_button.addEventListener("click", function () {
   console.log("Hai cliccato");
+
+  //funzione per mostrare le immagini alternate
+
+  turn_off.classList.toggle("d-none");
+  turn_on.classList.toggle("d-none");
 });
